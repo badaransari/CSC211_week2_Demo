9f98887bf400220a68252cb2976cee7f44253254
itlabs <itlabs@WHB019_16.it.farmingdale.edu>
2021-09-09 14:25:59 -0400

    add one dimension array with initial values

--- CSC211_week2_Demo/src/main/java/com/mycompany/csc211_week2_demo/NewClass.java
+++ CSC211_week2_Demo/src/main/java/com/mycompany/csc211_week2_demo/NewClass.java
@@ -11,5 +11,14 @@ package com.mycompany.csc211_week2_demo;
  */
 public class NewClass {
     
-    
+    public static void main(String[] args)
+    {
+        int[] arr= new int[5];
+        arr[0]=5;
+        arr[2]=10;
+        for (int i=0; i<5; i++){
+            System.out.println("Array element value:" + arr[i]);
+            
+        }
+    }
 }
