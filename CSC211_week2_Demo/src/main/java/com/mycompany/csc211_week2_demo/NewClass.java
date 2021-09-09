6cb58cb82663d5ac9dd2bfc4aa8de7989dcf0faf
itlabs <itlabs@WHB019_16.it.farmingdale.edu>
2021-09-09 14:19:11 -0400

    another one

---
+++ CSC211_week2_Demo/src/main/java/com/mycompany/csc211_week2_demo/NewClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.csc211_week2_demo;
+
+/**
+ *
+ * @author itlabs
+ */
+public class NewClass {
+    
+    
+}
